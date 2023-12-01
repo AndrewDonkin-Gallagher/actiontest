2722d30e8e96bc8339a5bd36019c2bf49973e044
Andrew Donkin <andrew.donkin@gallagher.com>
2023-12-01 22:09:09 +1300

    Fri Dec  1 22:09:08 NZDT 2023

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -36,20 +36,5 @@ git rm -rf .github action || true
 git status
 git commit -m "Asciidoctored $GITHUB_SHA"
 
-# If the action is being run into the GitHub Servers,
-# the checkout action (which is being used)
-# automatically authenticates the container using ssh.
-# If the action is running locally, for instance using https://github.com/nektos/act,
-# we need to push via https with a Personal Access Token
-# which should be provided by an env variable.
-# We can run the action locally using act with:
-#    act -s GITHUB_TOKEN=my_github_personal_access_token
-#if ! ssh -T git@github.com > /dev/null 2>/dev/null; then
-#    echo SSH failed.
-#    URL="https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-#    git remote remove origin
-#    git remote add origin "$URL"
-#fi
-
 echo Pushing
 git push -f --set-upstream origin gh-pages
