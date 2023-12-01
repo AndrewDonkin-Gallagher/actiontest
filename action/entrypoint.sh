466577c2da0dccbb3202e85279393015703352ba
Andrew Donkin <andrew.donkin@gallagher.com>
2023-12-01 22:33:02 +1300

    Fri Dec  1 22:33:01 NZDT 2023

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -14,7 +14,7 @@ echo Starting new gh-pages branch
 git checkout --orphan gh-pages "${GITHUB_SHA}"
 # git checkout "${GITHUB_SHA}" -B gh-pages
 
-find / -name "*mmdc*" -ls
+find / -name "*ruby*" -ls
 
 echo Plain
 asciidoctor -o index.html --verbose test.adoc || true
