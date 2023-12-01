1e1ec4d4f4740a623a8901c3ed59013214af5226
Andrew Donkin <andrew.donkin@gallagher.com>
2023-12-01 21:37:43 +1300

    Fri Dec  1 21:37:42 NZDT 2023

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -4,32 +4,33 @@
 set -e
 # Hush ownership complaints
 git config --global --add safe.directory /github/workspace
+
+echo Fetch:
 git fetch --all
 
 echo Deleting gh-pages branch
-git branch -D gh-pages || true
+git branch --quiet -D gh-pages 2>/dev/null || true
 echo Starting new gh-pages branch
 #git checkout --orphan gh-pages "${GITHUB_SHA}"
 git checkout "${GITHUB_SHA}" -B gh-pages
 
-echo plain
+echo Plain
 asciidoctor -o index.html --verbose test.adoc || true
 
-echo diagram
+echo Diagram
 asciidoctor -r asciidoctor-diagram -o index2.html --verbose test.adoc || true
 
-echo book
+echo Book
 asciidoctor-pdf -r asciidoctor-diagram -o book.pdf --verbose test.adoc || true
-
-echo "Adding *.html to gh-pages branch"
 
 # Git insists on these
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
 
 # echo '!index.html' >> .gitignore
+echo "Adding *.html to gh-pages branch"
 git add -f *.html book.pdf
-git rm -rf .github || true
+git rm -rf .github action || true
 git status
 git commit -m "Asciidoctored $GITHUB_SHA"
 
@@ -41,12 +42,12 @@ git commit -m "Asciidoctored $GITHUB_SHA"
 # which should be provided by an env variable.
 # We can run the action locally using act with:
 #    act -s GITHUB_TOKEN=my_github_personal_access_token
-if ! ssh -T git@github.com > /dev/null 2>/dev/null; then
-    echo SSH failed.
-    URL="https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-    git remote remove origin
-    git remote add origin "$URL"
-fi
+#if ! ssh -T git@github.com > /dev/null 2>/dev/null; then
+#    echo SSH failed.
+#    URL="https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+#    git remote remove origin
+#    git remote add origin "$URL"
+#fi
 
 echo Pushing
 git push -f --set-upstream origin gh-pages
