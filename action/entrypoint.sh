8aaa8c66fc746ae4c718b398d57d5b20b7cf2125
Andrew Donkin <andrew.donkin@gallagher.com>
2023-12-04 18:09:40 +1300

    First using custom publish action

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -3,16 +3,15 @@
 # Bail on error.
 set -e
 # Hush ownership complaints
-git config --global --add safe.directory /github/workspace
+#git config --global --add safe.directory /github/workspace
 
-echo Fetch:
-git fetch --all
+#echo Fetch:
+#git fetch --all
 
-echo Deleting gh-pages branch
-git branch --quiet -D gh-pages 2>/dev/null || true
-echo Starting new gh-pages branch
-git checkout --orphan gh-pages "${GITHUB_SHA}"
-# git checkout "${GITHUB_SHA}" -B gh-pages
+#echo Deleting gh-pages branch
+#git branch --quiet -D gh-pages 2>/dev/null || true
+#echo Starting new gh-pages branch
+#git checkout --orphan gh-pages "${GITHUB_SHA}"
 
 sed -i \
     -e 's@\[source,mermaid\]@[mermaid]@' \
@@ -21,27 +20,31 @@ sed -i \
     -e "s@%%DATE%%@$(date)@" \
     test.adoc
 
+D=_foo
+rm -r $D
+mkdir -p $D
+
 echo Diagram
-asciidoctor -r asciidoctor-diagram -o index.html --verbose test.adoc || true
+asciidoctor -r asciidoctor-diagram -o $D/index.html --verbose test.adoc || true
 
 echo Book
-asciidoctor-pdf -r asciidoctor-diagram -o book.pdf --verbose test.adoc || true
+asciidoctor-pdf -r asciidoctor-diagram -o $D/book.pdf --verbose test.adoc || true
 
 echo Multipage
-rm -rf paged
-asciidoctor-multipage -r asciidoctor-diagram -D paged --verbose test.adoc || true
+asciidoctor-multipage -r asciidoctor-diagram -D $D/paged --verbose test.adoc || true
+
 # Names of chapter html files start with underscores, which Jekyll does not preserve,
 # so the repo needs a .nojekyll in the root.
 
 # Git insists on these
-git config --global user.email "action@github.com"
-git config --global user.name "GitHub Action"
+#git config --global user.email "action@github.com"
+#git config --global user.name "GitHub Action"
 
-echo "Adding *.html assets book.pdf to gh-pages branch"
-git add -f *.html assets book.pdf paged
-git rm -rf .gitignore .github action || true
-git status
-git commit -m "Asciidoctored $GITHUB_SHA"
+#echo "Adding *.html assets book.pdf to gh-pages branch"
+#git add -f *.html assets book.pdf paged
+#git rm -rf .gitignore .github action || true
+#git status
+#git commit -m "Asciidoctored $GITHUB_SHA"
 
-echo Pushing
-git push -f --set-upstream origin gh-pages
+#echo Pushing
+#git push -f --set-upstream origin gh-pages
