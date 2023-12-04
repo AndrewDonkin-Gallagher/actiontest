0099f86b88adf63e6deeb6071ddf8fc9edd3474c
Andrew Donkin <andrew.donkin@gallagher.com>
2023-12-05 10:48:18 +1300

    enough of that DIY tarring Tue Dec  5 10:48:18 NZDT 2023

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -3,11 +3,6 @@
 # Bail on error.
 set -e
 
-# Arguments:
-# $1 == $INPUT_OUTPUT :  filename for tarball artifact
-
-[[ x"$INPUT_OUTPUT" != x ]]
-
 sed -i \
     -e 's@\[source,mermaid\]@[mermaid]@' \
     -e "s@%%GITHUB_RUN_NUMBER%%@$GITHUB_RUN_NUMBER@" \
@@ -15,25 +10,9 @@ sed -i \
     -e "s@%%DATE%%@$(date)@" \
     test.adoc
 
-# $$ is probably "1"
-T=${RUNNER_TEMP}/adoc.$$.tar
-ls -al $RUNNER_TEMP ||:
-mkdir -p $RUNNER_TEMP
-
 echo Building single-page HTML version of AsciiDoc
 asciidoctor -r asciidoctor-diagram -o index.html --verbose test.adoc || true
 
 echo Building multi-page HTML from AsciiDoc
 asciidoctor-multipage -r asciidoctor-diagram -D multipage -o multipage/index.html --verbose test.adoc || true
 
-echo Putting output into $INPUT_OUTPUT
-tar --dereference -cvf $T --exclude .asciidoctor .
-ls -alr $RUNNER_TEMP
-mv $T $INPUT_OUTPUT
-
-echo Artifact: $INPUT_OUTPUT
-ls -l $INPUT_OUTPUT
-
-echo ::warning warning
-echo ::error error
-echo ::info info
